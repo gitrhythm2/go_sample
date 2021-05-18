c4d32c3165b0c89e2b4993abf877b64b7098f452
Hiroyuki Hosokawa <simamaru@gmail.com>
2021-05-18 17:13:39 +0900

    colorのサンプルを追加

---
+++ color/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("hoge fuga chome")
+	color.Blue("hoge fuga chome")
+	color.Cyan("hoge fuga chome")
+	color.Magenta("hoge fuga chome")
+	color.Yellow("hoge fuga chome")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_sample
 
 go 1.16
+
+require github.com/fatih/color v1.11.0
